e6bcda752ba1a423c1a224408fcdba358fdd386d
huang kuang <s0936929599@yahoo.com.tw>
2021-01-11 09:07:46 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -6,13 +6,22 @@ class Ui_Form(object):
         Form.resize(640,480)
         self.horizontalLayout = QtWidgets.QHBoxLayout(Form)
         self.horizontalLayout.setObjectName("horizontalLayout")
+
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
+
         self.image_label = QtWidgets.QLabel(Form)
         self.image_label.setObjectName("image_label")
         self.verticalLayout.addWidget(self.image_label)
+        #test
+        self.image_label1 = QtWidgets.QLabel(Form)
+        self.image_label1.setObjectName("image_label1")
+
+        #self.verticalLayout.addWidget(self.image_label1)
+
         self.control_bt = QtWidgets.QPushButton(Form)
         self.control_bt.setObjectName("control_bt")
+
         self.verticalLayout.addWidget(self.control_bt)
         self.horizontalLayout.addLayout(self.verticalLayout)
 
@@ -21,6 +30,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Cam view"))
+        Form.setWindowTitle(_translate("Form", "Barcode Reader"))
         self.image_label.setText(_translate("Form", ""))
+        self.image_label1.setText(_translate("Form", "kuuk"))
         self.control_bt.setText(_translate("Form", "Start"))
