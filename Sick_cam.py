f8569264f3425cee42d55f285feee3d389b63df4
huang kuang <s0936929599@yahoo.com.tw>
2021-01-27 15:04:22 +0800

    Update Sick_cam.py

--- Sick_cam.py
+++ Sick_cam.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import PyQt5.QtGui
 # import some PyQt5 modules
 from PyQt5.QtWidgets import QApplication,QAction
@@ -17,20 +18,22 @@ from ui import *
 import numpy as np
 import threading
 import time
-import numpy as np
 import clr
 import ctypes
 clr.FindAssembly("sick_vision_api_dotnet.dll")
 clr.AddReference('sick_vision_api_dotnet')
 import vision_api 
+clr.FindAssembly('AXMVS100_dotNet.dll')
+clr.AddReference('AXMVS100_dotNet')
+import AXMVS100_dotNet
 from System import IntPtr
 import multiprocessing as mp
 
-bRuning,bGet,frame = True ,False ,None
+bRuning,bGet,frame,iRuning= True ,False ,None,True
 
-dataStream,deviceCount,count=0,0,0
+dataStream,deviceCount,count,image_s=0,0,0,0
 
-
+os.chdir('C:/Users/AX/Desktop/build/save_img')
 
 class MainWindow(QWidget):
     # class constructor
@@ -51,25 +54,38 @@ class MainWindow(QWidget):
         
         self.ui.image_label.setPixmap(QPixmap.fromImage(self.qImg))
 
-        # create a timer
-        self.timer = QTimer()
-        # set timer timeout callback function
-        #self.timer.timeout.connect(self.viewCam)
-        #self.timer.timeout.connect(self.viewCam_sick)
-        # set control_bt callback clicked  function
-
-        #self.ui.control_bt.clicked.connect(self.controlTimer)
+       
         self.ui.control_bt.clicked.connect(self.thread_sick_cam)
         
         
     #close window to call this funtion  
     def closeEvent(self, event):
-        global bRuning
+        global bRuning,iRuning
         bRuning=False
+        iRuning=False
         sys.exit()
+    def bt_saveimage(self):
+        global bRuning,image_s
+
+        device_ = AXMVS100_dotNet.AXMVS100();
+        result = device_.AXMVS100_LightingCtrl_SetConfig(1,0,0,0,0,0,0,1)
+        result = device_.AXMVS100_LightingCtrl_SetSignal(1,0,0,1)
+        while iRuning:
+            result1,status=device_.AXMVS100_DI_ReadLine(0,0,0)
+            number=1
+            while status==1:
+                while os.path.exists(f'{number}.jpg'):number+=1
+                cv2.imwrite(f'{number}.jpg',image_s)
+                result = device_.AXMVS100_LightingCtrl_Enable(1)
+                #print(f'Enable True:{result}')
+                time.sleep(0.15)
+                result = device_.AXMVS100_LightingCtrl_SetConfig(1,0,0,0,0,0,0,1)
+                #print(f'Enable False:{result}')
+                time.sleep(1)
+                status=0
 
     def thread_sick_cam(self):
-        global frame,bGet,bRuning,deviceCount,deviceManager,t1
+        global frame,bGet,bRuning,deviceCount,deviceManager,t1,image_s
         
         if self.ui.control_bt.text()=='Start':
 
@@ -86,23 +102,19 @@ class MainWindow(QWidget):
                 
             bRuning=True
             threading.Thread(target=self.sick_cam).start()
-            #close thread when main thread closing
-            #self.t1=threading.Thread(target=self.sick_cam)
-            #t1.setDaemon(True)
-            #self.t1.start()
-            #mp.Process(target=self.sick_cam).start()
-            time.sleep(3)
-            #print(self.ui.control_bt.text())
-            while self.ui.control_bt.text()=='Stop':
-                #print(bGet)
-                while bGet==False:time.sleep(0.01)
+            threading.Thread(target=self.bt_saveimage).start()
 
-                image = cv2.resize(frame,(640,480))
-                image=self.read_barcodes(image)
+            time.sleep(3)
+
+            while self.ui.control_bt.text()=='Stop':
+                while bGet==False:time.sleep(0.001)
+                bGet = False
+                image = cv2.resize(frame,(640,480)).reshape((480,640,1))
+                image=np.concatenate((image,image,image),axis=2)
+                image_s=self.read_barcodes(image)
                 #image=np.concatenate((image,image,image),axis=0)
                 #image=image.reshape(640,480,3)
-                #print(image.shape)
-                qImg = QImage(image.data, 640, 480, 1920, QImage.Format_RGB888)
+                qImg = QImage(image_s.data, 640, 480, 1920, QImage.Format_RGB888)
                 self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
                 QApplication.processEvents()
         else:
@@ -118,13 +130,7 @@ class MainWindow(QWidget):
         deviceManager = vision_api.DeviceManager.Instance()
         deviceManager.Update()
         deviceCount = deviceManager.Devices().Count
-        
-        #d = deviceManager.Devices()[i]
-        #print(f'{d.ModelName()}')
-        #print(f'{d.ParentInterface().DisplayName()}')
-        #print(f'{d.ParentInterface().ParentSystem().DisplayName()}')
-        #print(f'{d.ParentInterface().ParentSystem().Version()}')
-        #print(threading.active_count())
+
         if count==0:
             device = deviceManager.Devices()[0].OpenDevice(vision_api.core.DeviceAccessType.Control)
             dataStreams = device.DataStreams()
@@ -133,6 +139,10 @@ class MainWindow(QWidget):
             nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("UserSetSelector").SetCurrentEntry("Default")
             nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("UserSetLoad").Execute()
             nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("UserSetLoad").WaitUntilDone()
+            nodeFrameRate = nodeMapRemoteDevice.FindNode[vision_api.core.nodes.FloatNode]("AcquisitionFrameRate")
+            #print(f'FrameRate:{nodeFrameRate.Value()}\nMax:{nodeFrameRate.Maximum()}\nMin:{nodeFrameRate.Minimum()}')
+            nodeFrameRate.SetValue(30)
+            #print(nodeFrameRate.Value())
             #print(f'Model Name: {nodeMapRemoteDevice.FindNode[vision_api.core.nodes.StringNode]("DeviceModelName").Value()}')
             #print(f'User ID: {nodeMapRemoteDevice.FindNode[vision_api.core.nodes.StringNode]("DeviceUserID").Value()}')
             payloadSize = nodeMapRemoteDevice.FindNode[vision_api.core.nodes.IntegerNode]("PayloadSize").Value()
@@ -145,37 +155,38 @@ class MainWindow(QWidget):
             nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStart").Execute();
             nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStart").WaitUntilDone();
             count+=1
+            npArray = None
+            destPtr = None
             while bRuning:
-                time.sleep(0.1)
-                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(1000))
-                npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
-                destPtr = npArray.__array_interface__['data'][0]
+                while dataStream.NumBuffersAwaitDelivery()<1:time.sleep(0.001)
+                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(0))
+                if npArray is None:
+                    npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
+                    destPtr = npArray.__array_interface__['data'][0]
                 ctypes.memmove(destPtr, buffer.BasePtr().ToInt64(), npArray.nbytes)
-                #frame = npArray.reshape((buffer.Height(),buffer.Width(),1))
-                npArray=npArray.reshape((buffer.Height(),buffer.Width(),1))
-                frame=np.concatenate((npArray,npArray,npArray),axis=2)
+                frame = npArray.reshape((buffer.Height(),buffer.Width(),1))
                 dataStream.QueueBuffer(buffer);
                 bGet = True
-                #frameCounter += 1
-                #print(frameCounter)
+
         else:
+            npArray = None
+            destPtr = None
             while bRuning:
-                time.sleep(0.1)
-                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(1000))
-                npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
-                destPtr = npArray.__array_interface__['data'][0]
+                while dataStream.NumBuffersAwaitDelivery()<1:time.sleep(0.001)
+                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(0))
+                if npArray is None:
+                    npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
+                    destPtr = npArray.__array_interface__['data'][0]
                 ctypes.memmove(destPtr, buffer.BasePtr().ToInt64(), npArray.nbytes)
-                #frame = npArray.reshape((buffer.Height(),buffer.Width(),1))
-                npArray=npArray.reshape((buffer.Height(),buffer.Width(),1))
-                frame=np.concatenate((npArray,npArray,npArray),axis=2)
+                frame = npArray.reshape((buffer.Height(),buffer.Width(),1))
                 dataStream.QueueBuffer(buffer);
                 bGet = True
                 #frameCounter += 1
                     #print(frameCounter)
 
     # decode barcodes  
-    def read_barcodes(self,frame):
-        barcodes = pyzbar.decode(frame)
+    def read_barcodes(self,frame_1):
+        barcodes = pyzbar.decode(frame_1)
         #print(barcodes)
 
         for barcode in barcodes:
@@ -190,15 +201,15 @@ class MainWindow(QWidget):
             #data.append((x+w)/(y+h))
                 barcode_info = barcode.data.decode('utf-8')
 
-                cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2) #cv2.rectangle(影像, 頂點座標, 對向頂點座標, 顏色, 線條寬度) 
+                cv2.rectangle(frame_1, (x, y),(x+w, y+h), (0, 255, 0), 2) #cv2.rectangle(影像, 頂點座標, 對向頂點座標, 顏色, 線條寬度) 
                     
                     #2
                 font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 1.0, (255, 255, 255), 1) # cv2.putText(影像, 文字, 座標, 字型, 大小, 顏色, 線條寬度, 線條種類)
+                cv2.putText(frame_1, barcode_info, (x + 6, y - 6), font, 1.0, (255, 255, 255), 1) # cv2.putText(影像, 文字, 座標, 字型, 大小, 顏色, 線條寬度, 線條種類)
                 #cv2.putText(frame, str((x+w)/(y+h)), (x + 6, y - 3), font, 1.0, (255, 255, 255), 1)
             #with open("barcode_result.txt", mode ='w') as file:
             #    file.write("Recognized Barcode:" + barcode_info)
-        return frame
+        return frame_1
     # get form size
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -214,3 +225,4 @@ if __name__ == '__main__':
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec_())
+
