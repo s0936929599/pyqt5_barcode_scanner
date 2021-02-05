f814cc1356c02ec8fd043e27bf3bcdb8c6418026
huang kuang <s0936929599@yahoo.com.tw>
2021-02-05 14:05:15 +0800

    Update Sick_cam_event_detect.py

--- Sick_cam_event_detect.py
+++ Sick_cam_event_detect.py
@@ -25,15 +25,17 @@ clr.AddReference('sick_vision_api_dotnet')
 import vision_api 
 clr.FindAssembly('AXMVS100_dotNet.dll')
 clr.AddReference('AXMVS100_dotNet')
+import TriggerOut 
 import AXMVS100_dotNet
 from System import IntPtr
 import multiprocessing as mp
 from multiprocessing import Process, Queue
-bRuning,bGet,frame,iRuning,sRunning= True ,False ,None,True,False
+TriggerOut.main()
+bRuning,bGet,frame,iRuning,sRunning,get_image= True ,False ,None,True,False,True
 
 dataStream,deviceCount,count,image_s,status=0,0,0,0,0
 
-os.chdir('C:/Users/AX/Desktop/build/save_img')
+os.chdir('C:/Users/AX/Desktop/save_img')
 
 class MainWindow(QWidget):
     # class constructor
@@ -54,10 +56,11 @@ class MainWindow(QWidget):
         
         self.ui.image_label.setPixmap(QPixmap.fromImage(self.qImg))
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.image_update)
+        #self.timer = QTimer()
+        #self.timer.timeout.connect(self.image_update)
         self.ui.control_bt.clicked.connect(self.thread_sick_cam)
-        
+        #self.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
+        #self.setWindowFlags( QtCore.Qt.CustomizeWindowHint )
         
     #close window to call this funtion  
     def closeEvent(self, event):
@@ -67,30 +70,22 @@ class MainWindow(QWidget):
         sRunning=False
         sys.exit()
     def bt_saveimage(self): # button trigger light and pyzbar
-        global bRuning,image_s,iRuning,sRunning
-        device_ = AXMVS100_dotNet.AXMVS100();
-        result = device_.AXMVS100_LightingCtrl_SetConfig(1,0,0,0,0,0,0,1)
-        result = device_.AXMVS100_LightingCtrl_SetSignal(1,0,0,1)
-        while iRuning:
-            result1,status=device_.AXMVS100_DI_ReadLine(0,0,0)
-            number=1
-            while status==1:
-                while os.path.exists(f'{number}.jpg'):number+=1
-                sRunning=False
+        global bRuning,image_s,iRuning,sRunning,frame,bGet
+        while bRuning:
+            while bGet==False and bRuning:time.sleep(0.001)
+            bGet = False
+            #print(threading.active_count())
+            try:
+                image = cv2.resize(frame,(640,480)).reshape((480,640,1))
+                image_s=np.concatenate((image,image,image),axis=2)
                 image_s=self.read_barcodes(image_s)
                 qImg = QImage(image_s.data, 640, 480, 1920, QImage.Format_RGB888)
                 self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
                 QApplication.processEvents()
-                image_s=image_s[...,::-1] #BGR to RGB
-                cv2.imwrite(f'{number}.jpg',image_s)
-                result = device_.AXMVS100_LightingCtrl_Enable(1)
-                #print(f'Enable True:{result}')
-                time.sleep(0.15)
-                result = device_.AXMVS100_LightingCtrl_SetConfig(1,0,0,0,0,0,0,1)
-                #print(f'Enable False:{result}')
-                time.sleep(1)
-                status=0
-            sRunning=True
+            except:
+                pass
+
+        
 
     def image_update(self): #image update every second
         global frame,bGet,bRuning,deviceCount,deviceManager,t1,image_s,iRuning,status,sRunning
@@ -104,7 +99,7 @@ class MainWindow(QWidget):
             QApplication.processEvents()
 
     def thread_sick_cam(self):
-        global frame,bGet,bRuning,deviceCount,deviceManager,t1,image_s,iRuning,status,sRunning
+        global frame,bGet,bRuning,deviceCount,deviceManager,t1,image_s,iRuning,status,sRunning,get_image
         
         if self.ui.control_bt.text()=='Start':
 
@@ -113,102 +108,100 @@ class MainWindow(QWidget):
             self.ui.image_label1.setAlignment(PyQt5.QtCore.Qt.AlignCenter)   
             self.ui.image_label1.setFont(QFont('Times', 30,QFont.Bold))
             self.ui.image_label1.setStyleSheet('color: white')
-            self.ui.control_bt.setText("Stop")
+            self.ui.control_bt.setText("Connecting")
             QApplication.processEvents()
             self.ui.image_label1.setText('')
                
             bRuning=True 
             iRuning=True
             sRunning=True
-            threading.Thread(target=self.sick_cam).start()     
+            threading.Thread(target=self.harware_trigger_camera).start()     
             threading.Thread(target=self.bt_saveimage).start()
             
-            self.timer.start(1)
+            #self.timer.start(1)
             time.sleep(3)
-
-            #while self.ui.control_bt.text()=='Stop' and sRunning:
-            #    while bGet==False:time.sleep(0.001)
-            #    print(time.time())
-            #    bGet = False
-            #    image = cv2.resize(frame,(640,480)).reshape((480,640,1))
-            #    image_s=np.concatenate((image,image,image),axis=2)
-
-                #image_s=self.read_barcodes(image)
-                #image=np.concatenate((image,image,image),axis=0)
-                #image=image.reshape(640,480,3)
-            #    qImg = QImage(image_s.data, 640, 480, 1920, QImage.Format_RGB888)
-            #    self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
-            #    QApplication.processEvents()
+            self.ui.control_bt.setText("Stop")
+            
         else:
 
             bRuning=False #stop image thread
+            get_image=False
             iRuning=False #stop save thread
             sRunning=False
-            self.timer.stop()
             self.ui.control_bt.setText("Start")
-            QApplication.processEvents()
-    def sick_cam(self):
-        global bRuning,bGet,frame,dataStream,count,deviceCount
-        
+
+            #sys.exit()
+ 
+    def harware_trigger_camera(self):
+        global bRuning,bGet,frame,get_image,iRuning,sRunning
+
         frameCounter = 0
         vision_api.Library.Initialize()
         deviceManager = vision_api.DeviceManager.Instance()
         deviceManager.Update()
         deviceCount = deviceManager.Devices().Count
+        #d = deviceManager.Devices()[i]
+        #print(f'{d.ModelName()}')
+        #print(f'{d.ParentInterface().DisplayName()}')
+        #print(f'{d.ParentInterface().ParentSystem().DisplayName()}')
+        #print(f'{d.ParentInterface().ParentSystem().Version()}')
+        device = deviceManager.Devices()[0].OpenDevice(vision_api.core.DeviceAccessType.Control)
+        dataStreams = device.DataStreams()
+        dataStream = dataStreams[0].OpenDataStream()
+        nodeMapRemoteDevice = device.RemoteDevice().NodeMaps()[0]
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("UserSetSelector").SetCurrentEntry("Default")
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("UserSetLoad").Execute()
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("UserSetLoad").WaitUntilDone()
+        #print(f'Model Name: {nodeMapRemoteDevice.FindNode[vision_api.core.nodes.StringNode]("DeviceModelName").Value()}')
+        #print(f'User ID: {nodeMapRemoteDevice.FindNode[vision_api.core.nodes.StringNode]("DeviceUserID").Value()}')
+        payloadSize = nodeMapRemoteDevice.FindNode[vision_api.core.nodes.IntegerNode]("PayloadSize").Value()
+        bufferCountMax = dataStream.NumBuffersAnnouncedMinRequired()
+        #print(payloadSize,bufferCountMax)
+        for bufferCount in range(bufferCountMax):
+            buffer = dataStream.AllocAndAnnounceBuffer(payloadSize, IntPtr.Zero)
+            dataStream.QueueBuffer(buffer)
+        
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("TriggerSelector").SetCurrentEntry("ExposureStart")
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("TriggerMode").SetCurrentEntry("On")
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("TriggerSource").SetCurrentEntry("Line0")
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("TriggerActivation").SetCurrentEntry("RisingEdge")
+        dataStream.StartAcquisition()
+        nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStart").Execute()
+        #nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStart").WaitUntilDone()
 
-        if count==0:
-            device = deviceManager.Devices()[0].OpenDevice(vision_api.core.DeviceAccessType.Control)
-            dataStreams = device.DataStreams()
-            dataStream = dataStreams[0].OpenDataStream()
-            nodeMapRemoteDevice = device.RemoteDevice().NodeMaps()[0]
-            nodeMapRemoteDevice.FindNode[vision_api.core.nodes.EnumerationNode]("UserSetSelector").SetCurrentEntry("Default")
-            nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("UserSetLoad").Execute()
-            nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("UserSetLoad").WaitUntilDone()
-            nodeFrameRate = nodeMapRemoteDevice.FindNode[vision_api.core.nodes.FloatNode]("AcquisitionFrameRate")
-            #print(f'FrameRate:{nodeFrameRate.Value()}\nMax:{nodeFrameRate.Maximum()}\nMin:{nodeFrameRate.Minimum()}')
-            nodeFrameRate.SetValue(30)
-            #print(nodeFrameRate.Value())
-            #print(f'Model Name: {nodeMapRemoteDevice.FindNode[vision_api.core.nodes.StringNode]("DeviceModelName").Value()}')
-            #print(f'User ID: {nodeMapRemoteDevice.FindNode[vision_api.core.nodes.StringNode]("DeviceUserID").Value()}')
-            payloadSize = nodeMapRemoteDevice.FindNode[vision_api.core.nodes.IntegerNode]("PayloadSize").Value()
-            bufferCountMax = dataStream.NumBuffersAnnouncedMinRequired()
-            #bRuning=Trueprint(payloadSize,bufferCountMax)
-            for bufferCount in range(bufferCountMax):
-                buffer = dataStream.AllocAndAnnounceBuffer(payloadSize, IntPtr.Zero)
-                dataStream.QueueBuffer(buffer)
-            dataStream.StartAcquisition();
-            nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStart").Execute();
-            nodeMapRemoteDevice.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStart").WaitUntilDone();
-            count+=1
-            npArray = None
-            destPtr = None
-            while bRuning:
-                while dataStream.NumBuffersAwaitDelivery()<1:time.sleep(0.01)
-                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(0))
-                if npArray is None:
-                    npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
-                    destPtr = npArray.__array_interface__['data'][0]
+        time.sleep(1)
+
+        while bRuning:
+            
+            for t0 in range(1000): 
+                if dataStream.NumBuffersAwaitDelivery()>0 or bRuning==False :break
+                else:time.sleep(0.001)
+            #while dataStream.NumBuffersAwaitDelivery()<1 and get_image :time.sleep(0.01)
+            if bRuning==False:
+                remoteNodeMap = device.RemoteDevice().NodeMaps()[0];
+                remoteNodeMap.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStop").Execute();
+                remoteNodeMap.FindNode[vision_api.core.nodes.CommandNode]("AcquisitionStop").WaitUntilDone();
+                dataStream.KillWait();
+                dataStream.StopAcquisition(vision_api.core.AcquisitionStopMode.Default);
+                dataStream.Flush(vision_api.core.DataStreamFlushMode.DiscardAll);
+                for buffer in dataStream.AnnouncedBuffers():
+                         
+                    dataStream.RevokeBuffer(buffer);
+                vision_api.Library.Close();
+                break
+            try:
+                
+                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(1000))
+                npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
+                destPtr = npArray.__array_interface__['data'][0]
                 ctypes.memmove(destPtr, buffer.BasePtr().ToInt64(), npArray.nbytes)
                 frame = npArray.reshape((buffer.Height(),buffer.Width(),1))
                 dataStream.QueueBuffer(buffer);
                 bGet = True
-
-        else:
-            npArray = None
-            destPtr = None
-            while bRuning:
-                while dataStream.NumBuffersAwaitDelivery()<1:time.sleep(0.01)
-                buffer = dataStream.WaitForFinishedBuffer(vision_api.core.Timeout(0))
-                if npArray is None:
-                    npArray = np.empty(buffer.Size(), order='C', dtype=np.dtype('uint8'))
-                    destPtr = npArray.__array_interface__['data'][0]
-                ctypes.memmove(destPtr, buffer.BasePtr().ToInt64(), npArray.nbytes)
-                frame = npArray.reshape((buffer.Height(),buffer.Width(),1))
-                dataStream.QueueBuffer(buffer);
-                bGet = True
-                #frameCounter += 1
-                    #print(frameCounter)
-
+                frameCounter += 1
+                #print(frameCounter)
+            except:
+                pass
     # decode barcodes  
     def read_barcodes(self,frame_1):
         barcodes = pyzbar.decode(frame_1)
